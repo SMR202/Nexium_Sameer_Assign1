0365814701cfad3694ccfb6bace4279125cdb077
SMR202 <sameeramjad202@gmail.com>
2025-07-06 22:18:16 +0500

    added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+      <link rel="icon" href="/quote.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
